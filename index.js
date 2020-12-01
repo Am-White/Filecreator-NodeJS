4bb1b28d5893929d185ec9f0b3f0b41a43e59986
littlefire <aybwhite@gmail.com>
2020-11-30 23:10:28 -0800

    copting baseline index.js from in class mini project

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// function to write README file
-function writeToFile(fileName, data) {
+
+function promptUser() {
+ return inquirer.prompt ([
+        {
+           type: "input",
+           message: "What is your name?", 
+           name: "name",
+        },
+        {
+            type: "input",
+            message: "Where are you from?", 
+            name: "place",
+         },
+         {
+            type: "input",
+            message: "What would you like your heading to say?", 
+            name: "heading",
+         },
+         {
+            type: "input",
+            message: "What is your GitHub username?", 
+            name: "username",
+         },
+         {
+            type: "input",
+            message: "What is your LinkedIn?", 
+            name: "link",
+            
+         },
+ ]);
+}
+    
+//Creating inner code
+function generateMarkdown(response) {
+    return `
+    
+    `
 }
 
-// function to initialize program
-function init() {
+//Initialize program
+//async before a function = function always returns a promise.
+async function init() {
+    //In complete honestly I am confused about try element
+    try {
+        // await makes JavaScript wait until that promise settles and returns its result.
+        const response = await promptUser();
 
+        const readME = generateMarkdown(response);
+
+        await writeFileAsync ("README.md", readME;
+        console.log("success!");
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-// function call to initialize program
 init();
